4f77d4e7881a1728ea6d2dcd5b3579e9ba131e82
Pannawit <pannawit.p@ku.th>
2022-09-03 14:13:29 +0700

    first commit

---
+++ src/main/java/th/ac/ku/restaurant/controller/HomeController.java
@@ -0,0 +1,14 @@
+package th.ac.ku.restaurant.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Sawaddee");
+// ต้องคืนค่าเป็นชื่อไฟล์ html template โดยในเมธอดนคืนค่าเป็น home.html
+        return "home";
+    }
+}
+
